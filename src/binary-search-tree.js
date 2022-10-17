d5f6bd5c69d04e066c7f09ce9ac671331a6e9730
Valeria <valeria.yanycheva@gmail.com>
2022-10-18 02:34:58 +0600

    feat: all tasks are done

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -174,21 +174,6 @@ class BinarySearchTree {
   }
 }
 
-const tree = new BinarySearchTree();
-      tree.add(9);
-      tree.add(14);
-      tree.add(2);
-      tree.add(6);
-      tree.add(128);
-      tree.add(8);
-      tree.add(31);
-      tree.add(54);
-      tree.add(1);
-      tree.remove(14);
-      tree.remove(8);
-      tree.remove(9);
-      tree.has(54);
-
 module.exports = {
   BinarySearchTree
 };
